b5a30de16268a6784b83d7b605ae817417582702
Portal Tester <portaltester@stp7cor4542ltm.fios-router.home>
2017-05-13 21:28:42 -0400

    updates

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,17 +1,18 @@
 'use strict'
 import React from 'react';
-import {Link} from 'react-router-dom';
+import{NavLink} from 'react-router-dom';
 export default class Header extends React.Component{
     render(){
         return(
             <nav className='navbar navbar-default'>
                 <div className='container-fluid'>
-                    <Link to='/' className='navbar-brand'>
+                    <NavLink to='/' className='navbar-brand'>
                     <img src='images/pluralsight-logo.png'/>
-                    </Link>
+                    </NavLink>
                     <ul className='nav navbar-nav'>
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/about'>About</Link></li>
+                        <li><NavLink to='/' activeClassName='active'>Home</NavLink></li>
+                        <li><NavLink to='/about' activeClassName='active'>About</NavLink></li>
+                        <li><NavLink to='/courses' activeClassName='active'>Courses</NavLink></li>
                     </ul>
                 </div>
                 </nav>
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from './components/app';
 import { HashRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Home from './components/home/homePage';
 import About from './components/about/aboutPage';
+import Courses from './components/course/coursesPage';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 const supportsHistory = 'pushState' in window.history;
@@ -16,6 +17,7 @@ ReactDOM.render( <HashRouter baseName='/app' forceRefresh={!supportsHistory}>
       <Switch>
         <Route  exact path='/' component={Home} />
         <Route  path='/about' component={About}/>
+        <Route path='/courses' component={Courses} />
         <Redirect from="about-us" to="about" />
         <Redirect from="about/*" to="about" />
      </Switch>
